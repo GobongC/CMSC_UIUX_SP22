c4e4a0df803035b3d3c57efe20de793922b0f0c3
GobongC <56932388+GobongC@users.noreply.github.com>
2022-04-08 13:29:44 -0400

    Added Local Multiplayer Mode

--- Dots-and-Boxes-3D/Assets/Scriptes/MainMenu.cs
+++ Dots-and-Boxes-3D/Assets/Scriptes/MainMenu.cs
@@ -5,7 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
+    Vector3 myVector;
+
+
     public Main main;
+    
         public void PlayGame ()
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -24,16 +29,22 @@ public class MainMenu : MonoBehaviour
         public void changeGrid5 () { 
             Main.Width = 5;
             Main.Height = 5;
+            myVector = new Vector3(3f,4f,2.25f);
+            //CameraPosition.move(myVector);
         }
 
         public void changeGrid6 () { 
             Main.Width = 6;
             Main.Height = 6;
+            myVector = new Vector3(3f,4f,2.25f);
+            //CameraPosition.move(myVector);
         }
 
         public void changeGrid7 () { 
             Main.Width = 7;
             Main.Height = 7;
+            myVector = new Vector3(3f,4f,2.25f);
+            //CameraPosition.move(myVector);
         }
 
 }
