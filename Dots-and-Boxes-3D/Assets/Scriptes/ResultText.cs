9768494b1483d4a295125f1f31d408184c7a99f7
GobongC <56932388+GobongC@users.noreply.github.com>
2022-05-02 13:00:11 -0400

    Results Scripting

--- Dots-and-Boxes-3D/Assets/Scriptes/ResultText.cs
+++ Dots-and-Boxes-3D/Assets/Scriptes/ResultText.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ResultText : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    RuntimeData runtimeData = new RuntimeData();
+    public TextMeshProUGUI rScore;
+    
+    
+   void Update()
+   {
+   if (Score.playerScore>Score.enemyScore)
+   {
+        rScore.text = ("You Won!");
+   }
+   else if(Score.playerScore == Score.enemyScore)
+   {
+        rScore.text = ("It's a tie!");
+   }
+   else
+   {
+        rScore.text = ("You Lost!");
+   }
+   
+   }
 }
